b3b3bc406680ad3d383e52344bcc7b719b496407
gdevivo30 <64308491+gdevivo30@users.noreply.github.com>
2020-05-06 15:57:11 +0200

    Create esercizio3.py

---
+++ Esercizio3/gdevivo30/esercizio3.py
@@ -0,0 +1,63 @@
+import csv
+import sys
+import math
+import string
+
+lbldict = {}
+dbdict = {}
+affini =[]
+
+with open(sys.argv[1]) as csv_file:
+    csv_reader=csv.reader(csv_file, delimiter=',')
+    line_count=0
+    for row in csv_reader:
+        if line_count == 0:
+            for i, lbl in enumerate(row):
+                lbldict.update({i : lbl})
+            line_count += 1
+        else:
+            dbdict.update({row[0]:row})
+            line_count += 1
+
+for i in dbdict:
+    li=dbdict[i]
+    liDV=[]
+    for j in dbdict:
+        lj=dbdict[j]
+        if li[0] == lj[0]:
+            continue
+        k = 1 
+        s = 0
+        while k < len(li):
+            s += (int(li[k])-int(lj[k]))**2
+            k += 1
+        if s == 0.0:
+            continue
+        liDV.append((li[0]+" "+lj[0], math.sqrt(s)))
+
+    t0=(" ", 0.0)
+    for k, t in enumerate(sorted(liDV, key=lambda t: t[1])):
+        if k == 0:
+            t0=t
+            affini.append(t[0].split())
+            continue
+        if t0[1] < t[1]:
+            break
+        affini.append(t[0].split())
+
+mproduct = {}
+
+for cpl in affini:
+    for k, v1 in enumerate(dbdict[cpl[0]]):
+        if k == 0:
+            continue
+        if v1 < dbdict[cpl[1]][k]:
+            w=cpl[0]+" "+lbldict[k]
+            if w in mproduct.keys():
+                v=mproduct[w]
+            else:
+                v=0
+            mproduct.update({w : v+1})
+
+for i in mproduct:
+    print("a ", i.split()[0], " manca ", i.split()[1])
