7f59bdc6727dc906f983bb5f9b95290f8f0230b2
Burlesco70 <mario.nardi.70@gmail.com>
2020-05-23 18:48:54 +0200

    Svolgimento esercizio 7

--- Esercizio7/Burlesco/tamagotchi.py
+++ Esercizio7/Burlesco/tamagotchi.py
@@ -1,16 +1,20 @@
 from random import randrange
 
+
 class Pet():
     boredom_decrement = 4
     hunger_decrement = 6
     boredom_threshold = 5
     hunger_threshold = 10
     sounds = ['Mrrp']
-    def __init__(self, name = "Kitty"):
+
+    def __init__(self, name="Kitty"):
         self.name = name
         self.hunger = randrange(self.hunger_threshold)
         self.boredom = randrange(self.boredom_threshold)
-        self.sounds = self.sounds[:]  # copy the class attribute, so that when we make changes to it, we won't affect the other Pets in the class
+        # copy the class attribute, so that when we make changes to it,
+        # we won't affect the other Pets in the class
+        self.sounds = self.sounds[:]
 
     def clock_tick(self):
         self.boredom += 1
@@ -27,7 +31,6 @@ class Pet():
     def __str__(self):
         state = "     I'm " + self.name + ". "
         state += " I feel " + self.mood() + ". "
-        # state += "Hunger %d Boredom %d Words %s" % (self.hunger, self.boredom, self.sounds)
         return state
 
     def hi(self):
@@ -47,6 +50,7 @@ class Pet():
     def reduce_boredom(self):
         self.boredom = max(0, self.boredom - self.boredom_decrement)
 
+
 class Dog(Pet):
     # in the Dog class, Dog pets should express their hunger and boredom differently than generic Pets
     def mood(self):
@@ -56,6 +60,7 @@ class Dog(Pet):
             return "hungry already, arrrf"
         else:
             return "bored, so you should play with me"
+
 
 class Cat(Pet):
     # in the Cat class, cats express their hunger and boredom a little differently, too. They also have an extra instance, variable meow_count.
@@ -76,12 +81,14 @@ class Cat(Pet):
         else:
             return "a bit bored"
 
+
 class Lab(Dog):
     def fetch(self):
         return "I found the tennis ball!"
 
     def hi(self):
         print(self.sounds[randrange(len(self.sounds))] + self.fetch())
+
 
 class Poodle(Dog):
     def dance(self):
@@ -91,17 +98,22 @@ class Poodle(Dog):
         print(self.dance())
         Dog.hi(self)
 
+
 class Bird(Pet):
     sounds = ["chirp"]
+
     def __init__(self, name="Kitty", chirp_number=2):
-        Pet.__init__(self, name) # call the parent class's constructor
-        # basically, call the SUPER -- the parent version -- of the constructor, with all the parameters that it needs.
-        self.chirp_number = chirp_number # now, also assign the new instance variable
+        Pet.__init__(self, name)  # call the parent class's constructor
+        # basically, call the SUPER -- the parent version -- of the
+        # constructor, with all the parameters that it needs.
+        # now, also assign the new instance variable
+        self.chirp_number = chirp_number
 
     def hi(self):
         for i in range(self.chirp_number):
             print(self.sounds[randrange(len(self.sounds))])
         self.reduce_boredom()
+
 
 class Tiger(Cat):
     def __init__(self, name="Fluffy", meow_number=5):
@@ -115,26 +127,31 @@ class Tiger(Cat):
     def roar(self):
         print('ROOOOOAR!')
 
+
 class Retriever(Lab):
     def fetch(self):
         return "I found the tennis ball! I can fetch anything!"
 
 ###################################################
 
+
 def whichone(petlist, name):
     for pet in petlist:
         if pet.name == name:
             return pet
-    return None # no pet matched
+    return None  # no pet matched
 
-pet_types = {'dog': Dog, 'lab': Lab, 'poodle': Poodle, 'cat': Cat, 'bird': Bird, 'tiger':Tiger, 'retriever':Retriever}
+
+pet_types = {'dog': Dog, 'lab': Lab, 'poodle': Poodle, 'cat': Cat, 'bird': Bird,
+             'tiger': Tiger, 'retriever': Retriever}
+
+
 def whichtype(adopt_type="general pet"):
     return pet_types.get(adopt_type.lower(), Pet)
 
+
 def play():
     animals = []
-
-    option = ""
     base_prompt = """
         Quit
         Adopt <petname_with_no_spaces> <pet_type - choose dog, cat, lab, poodle, bird, or another unknown pet type>
@@ -186,15 +203,12 @@ def play():
             else:
                 pet.feed()
         else:
-            feedback+= "I didn't understand that. Please try again."
+            feedback += "I didn't understand that. Please try again."
 
         for pet in animals:
             pet.clock_tick()
             feedback += "\n" + pet.__str__()
 
+
 if __name__ == "__main__":
-    #play()
-    t = Tiger()
-    t.hi()
-    r = Retriever()
-    print(r.fetch())
+    play()
--- Esercizio7/Burlesco/test_tamagotchi.py
+++ Esercizio7/Burlesco/test_tamagotchi.py
@@ -1,4 +1,6 @@
-from tamagotchi import Pet, Cat, Dog, Bird, Lab, Poodle, Tiger, Retriever, whichone, whichtype
+from tamagotchi import Pet, Cat, Tiger, Retriever
+from tamagotchi import whichone, whichtype
+
 
 def test_pet():
     p = Pet()
@@ -6,6 +8,12 @@ def test_pet():
     assert p.hunger <= Pet.hunger_threshold
     assert p.boredom <= Pet.boredom_threshold
     assert 'Mrrp' in p.sounds
+
+
+def test_pet_str():
+    p = Pet()
+    assert str(p) == "     I'm Kitty.  I feel happy. "
+
 
 def test_pet_clock_tick():
     p = Pet()
@@ -15,23 +23,43 @@ def test_pet_clock_tick():
     assert p.boredom == before_boredom + 1
     assert p.hunger == before_hunger + 1
 
+
 def test_pet_mood():
     p = Pet()
-    while p.hunger < Pet.hunger_threshold:
+    while p.hunger <= Pet.hunger_threshold:
         p.clock_tick()
     assert p.mood() != "happy"
+
+
+def test_whichone():
+    petlist = []
+    romeo = Cat("Romeo")
+    petlist.append(romeo)
+    giulietta = Cat("Giulietta")
+    petlist.append(giulietta)
+    assert romeo == whichone(petlist, "Romeo")
+
+
+def test_whichtype():
+    assert whichtype("cat") == Cat
+    assert whichtype("tiger") == Tiger
+
 
 # Large task to cover all the cases!!!
 # To BE CONTINUED...
 
 # Test the new part (excercise)
 
-# capsys to caputure "print" output; captured is a namedtuple with "out" and "err"
+# capsys to caputure "print" output
+# captured is a namedtuple with "out" and "err"
+
+
 def test_tiger(capsys):
     t = Tiger()
     t.roar()
     captured = capsys.readouterr()
     assert captured[0] == 'ROOOOOAR!\n'
+
 
 def test_retriever():
     r = Retriever()
